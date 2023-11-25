77032604a85024dfca83bf71b13a491077da86a8
Tyfyter <tyfyter2616@gmail.com>
2023-11-25 06:13:17 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Input;
+
+namespace EdgyDeflector {
+	class Program {
+		static void Main(string[] args) {
+			string str = args[1].Replace("microsoft-edge:?url=", "");
+			if (str != args[1]) Process.Start(Uri.UnescapeDataString(str));
+		}
+	}
+}
